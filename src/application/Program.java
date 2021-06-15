dffac3aa5e285d3f8b87e9501f6f6a9b76e49f07
Yolanda Ornellas <ymf.ornellas@icloud.com>
2021-06-15 10:22:30 -0300

    Implements findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,15 +10,14 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
-		Department dep = new Department(1, "Books");
-		System.out.println(dep);
-		
-		Seller seller = new Seller(1, "Yolanda", "bla@bla.com.br", new Date(), 3000.0, dep);
-		System.out.println(seller);
 
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
+		
+		
 	}
 
 }
